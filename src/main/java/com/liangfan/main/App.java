4da957b0419bc44f50ca03f19f450e62b3f29766
宋亮亮 <807858227@qq.com>
2018-07-16 22:08:50 +0800

    1.init

---
+++ src/main/java/com/liangfan/main/App.java
@@ -0,0 +1,13 @@
+package com.liangfan.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
